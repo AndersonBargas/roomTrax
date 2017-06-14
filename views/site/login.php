92c78f8f2d87ed3f05408fbfd223d3b1a4e6677a
Anderson Bargas <bargas.anderson@gmail.com>
2017-06-13 21:24:00 -0300

    Página de login.

--- views/site/login.php
+++ views/site/login.php
@@ -10,38 +10,31 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<?php $form = ActiveForm::begin([
+    'fieldConfig' => [
+            'template' => "{input}",
+    ],
+]); ?>
 
-    <p>Please fill out the following fields to login:</p>
+<h1>Login</h1>
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]); ?>
+<?= $form->field($model, 'username')->textInput(['autofocus' => true, 'class' => 'form-control', 'required' => true, 'placeholder' => 'Usuário']) ?>
 
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+<?= $form->field($model, 'password')->passwordInput(['class' => 'form-control', 'required' => true, 'placeholder' => 'Senha']) ?>
 
-        <?= $form->field($model, 'password')->passwordInput() ?>
+<div class="clearfix"></div>
 
-        <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
+<div class="separator">
 
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-            </div>
-        </div>
+    <div class="clearfix"></div>
+    <br />
 
-    <?php ActiveForm::end(); ?>
-
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+    <div>
+        <h1><i class="fa fa-hourglass"></i> RoomTrax!</h1>
+        <p>©<?= date('Y') ?> <?= Yii::powered() ?></p>
     </div>
 </div>
+
+<?php ActiveForm::end(); ?>
+
+    
