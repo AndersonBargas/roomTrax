2e5328b70a96baceb8fad6e6f658b8fe4a3899fe
Anderson Bargas <bargas.anderson@gmail.com>
2017-06-15 19:24:32 -0300

    Novos assets para o DatePicker.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,17 +21,23 @@ class AppAsset extends AssetBundle
         'vendors/bootstrap/dist/css/bootstrap.min.css', // Bootstrap
         'vendors/font-awesome/css/font-awesome.min.css', // Font Awesome
         'vendors/nprogress/nprogress.css', // NProgress
+        'vendors/fullcalendar/dist/fullcalendar.min.css', // FullCalendar
+        'vendors/fullcalendar/dist/fullcalendar.print.css', // FullCalendar-
         'vendors/iCheck/skins/flat/green.css', // iCheck
         'vendors/switchery/dist/switchery.min.css', // Switchery
         'vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css', // bootstrap-progressbar
         'vendors/jqvmap/dist/jqvmap.min.css', // JQVMap
         'vendors/bootstrap-daterangepicker/daterangepicker.css', // bootstrap-daterangepicker
-        'css/custom.min.css', // Custom Theme Style
+        'vendors/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css', // DateTimePicker
+        'css/custom.css', // Custom Theme Style
     ];
     public $js = [
         //'vendors/jquery/dist/jquery.min.js', // jQuery
         'vendors/bootstrap/dist/js/bootstrap.min.js', // Bootstrap
         'vendors/fastclick/lib/fastclick.js', // FastClick
+        'vendors/moment/min/moment.min.js', // Moment
+        'vendors/moment/locale/pt-br.js', // Moment
+        'vendors/fullcalendar/dist/fullcalendar.min.js', // FullCalendar
         'vendors/nprogress/nprogress.js', // NProgress
         'vendors/bootbox/bootbox.min.js', // BootBox
         'vendors/Chart.js/dist/Chart.min.js', // Chart.js
@@ -54,6 +60,7 @@ class AppAsset extends AssetBundle
         'vendors/jqvmap/examples/js/jquery.vmap.sampledata.js', // JQVMap
         'vendors/moment/min/moment.min.js', // bootstrap-daterangepicker
         'vendors/bootstrap-daterangepicker/daterangepicker.js', // bootstrap-daterangepicker
+        'vendors/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js', // DateTimePicker
         'js/custom.js', // Custom Theme Scripts
 
     ];
