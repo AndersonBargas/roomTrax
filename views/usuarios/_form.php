2dbf4e04a9545ec47f5c85acea9c2eed86b9a7ca
Anderson Bargas <bargas.anderson@gmail.com>
2017-06-14 22:27:15 -0300

    Arquivos para o gerenciamento CRUD dos usuários.

---
+++ views/usuarios/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Usuarios */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="usuarios-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'senha')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'chave')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'dataCriacao')->textInput() ?>
+
+    <?= $form->field($model, 'dataAtualizacao')->textInput() ?>
+
+    <?= $form->field($model, 'dataConfirmacao')->textInput() ?>
+
+    <?= $form->field($model, 'dataExclusao')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
